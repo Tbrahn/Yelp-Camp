d5c30e0ffec885fdfa0e528b3e6ccebc658e9526
Tanner Dale Branham <tbranham9323@gmail.com>
2019-03-05 15:31:29 +0000

    update database

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Tbrahn:uKnkmoIQPMgHmyFc@cluster0-uftkw.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
